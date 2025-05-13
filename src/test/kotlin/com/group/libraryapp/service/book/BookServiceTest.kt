c8a5ed12f36c46e75261d72c24e4dfd915fa70a8
parkhyeongju_macmini <hyeongju1122@gmail.com>
2025-05-14 00:17:28 +0900

    :sparkles: 도서 대출 현황과 Left Join을 활용한 Kotlin 코드 작성

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -2,10 +2,12 @@ package com.group.libraryapp.service.book
 
 import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.book.BookType
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
@@ -34,7 +36,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 등록이 정상 작동한다.")
     fun saveBookTest() {
         // given
-        val request = BookRequest("처음 시작하는 FastAPI")
+        val request = BookRequest("처음 시작하는 FastAPI", BookType.COMPUTER)
 
         // when
         bookService.saveBook(request)
@@ -43,13 +45,14 @@ class BookServiceTest @Autowired constructor(
         val books = bookRepository.findAll()
         assertThat(books).hasSize(1)
         assertThat(books.first().name).isEqualTo(request.name)
+        assertThat(books.first().type).isEqualTo(request.type)
     }
 
     @Test
     @DisplayName("책 대출이 정상 동작한다.")
     fun loanBookTest() {
         // given
-        bookRepository.save(Book("처음 시작하는 FastAPI"))
+        bookRepository.save(Book.fixture("처음 시작하는 FastAPI"))
         val savedUser = userRepository.save(User("박형주", 22))
         val request = BookLoanRequest("박형주", "처음 시작하는 FastAPI")
 
@@ -63,16 +66,16 @@ class BookServiceTest @Autowired constructor(
         assertThat(results[0].user.id).isEqualTo(savedUser.id)
         assertThat(results[0].user.name).isEqualTo(savedUser.name)
         assertThat(results[0].user.age).isEqualTo(savedUser.age)
-        assertThat(results[0].isReturn).isFalse
+        assertThat(results[0].status).isEqualTo(UserLoanStatus.LOANED)
     }
 
     @Test
     @DisplayName("책이 진작 대출되어 있다면 신규 대출에 실패한다.")
     fun loanBookFailTest() {
         // given
-        bookRepository.save(Book("처음 시작하는 FastAPI"))
+        bookRepository.save(Book.fixture("처음 시작하는 FastAPI"))
         val savedUser = userRepository.save(User("박형주", 22))
-        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "처음 시작하는 FastAPI", false))
+        userLoanHistoryRepository.save(UserLoanHistory.fixture(savedUser, "처음 시작하는 FastAPI"))
         val request = BookLoanRequest("박형주", "처음 시작하는 FastAPI")
 
 
@@ -87,9 +90,9 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 반납이 정상 동작한다.")
     fun returnBook() {
         // given
-        bookRepository.save(Book("처음 시작하는 FastAPI"))
+        bookRepository.save(Book.fixture("처음 시작하는 FastAPI"))
         val savedUser = userRepository.save(User("박형주", 22))
-        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "처음 시작하는 FastAPI", false))
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "처음 시작하는 FastAPI"))
         val request = BookReturnRequest("박형주", "처음 시작하는 FastAPI")
 
         // when
@@ -98,6 +101,6 @@ class BookServiceTest @Autowired constructor(
         // then
         val results = userLoanHistoryRepository.findAll()
         assertThat(results).hasSize(1)
-        assertThat(results[0].isReturn).isTrue
+        assertThat(results[0].status).isEqualTo(UserLoanStatus.RETURNED)
     }
 }
